162fd6773f3176d9520c0d51b7c8b77620aacc4e
Mike Jurie <s3785631@student.rmit.edu.au>
2020-09-03 15:13:15 +1000

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+void exit();
+void showMenu();
+
 int main(int argc, char** argv) {
-    
+    std::cout << "Welcome to Azul!" << 
+    std::endl << "---------------------" << 
+    std::endl;
+    showMenu();
+
     return EXIT_SUCCESS;
+}
+
+void exit() {
+    // DELETE ALL OBJECTS
+    // CLEANUP
+}
+
+void showMenu() {
+    std::cout << std::endl << "Menu" << 
+    std::endl << "----" << 
+    std::endl << "1. New Game" <<
+    std::endl << "2. Load Game" <<
+    std::endl << "3. Credits" <<
+    std::endl << "4. Quit" <<
+    std::endl;
 }
