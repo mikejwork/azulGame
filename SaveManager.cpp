70ca0e6fd243f999645f8af0d64edbcc0cdc5857
Mike Jurie <s3785631@student.rmit.edu.au>
2020-09-04 10:30:20 +1000

    fixing savemanager

--- SaveManager.cpp
+++ SaveManager.cpp
@@ -3,6 +3,8 @@
 // We have to determine a structure of how the data is stored within the save file.
 // Player names are easy but things like the mosaic are gonna be fkn difficult
 
+// If u dont know what tf this is look at week 5 lecture stuff
+
 SaveManager::SaveManager() {
     saveCount = 0;
 }
@@ -14,7 +16,7 @@ void SaveManager::NewSave(std::string saveName) {
     for (int i = 0; i < 6; i++) {
         if (out.good()) {
             // Write save file
-            out << i << std::endl;
+            out << i << std::endl; // Causes issue with blank line at end, not sure if we can hardcode yet -- TODO
         }
     }
     out.close(); 
@@ -25,7 +27,7 @@ void SaveManager::LoadSave(std::string saveName) {
     std::string name = saveName + ".txt"; // adding extension (ease of use for the user)
     std::string line;
     in.open(name);
-    for (int i = 0; i < 6; i++) {
+    while (!in.eof()) {
         if (in.good()) {
             // Read save file
             in >> line;
